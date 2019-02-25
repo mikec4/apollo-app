166a49090d95224364f66f586abbdf349df5ea3c
ntiriniga <clarantiriniga@gmail.com>
2019-02-25 07:59:52 +0300

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const {ApolloServer, gql} = require('apollo-server-express');
+const {ApolloServer, gql,graphiqlExpress,graphqlExpress} = require('apollo-server-express');
+const {makeExecutableSchema} = require('graphql-tools');
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 require('./config');
 
 const {typeDefs} = require('./schema/schema');
@@ -28,10 +30,20 @@ const context = async ({req})=>{
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context
+    context,
+    introspection:true,
+    playground:true
 });
 
 server.applyMiddleware({app});
+// const schema = makeExecutableSchema({
+// typeDefs,
+// resolvers,
+// context
+// });
+//app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+//app.use('/', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 app.listen(process.env.PORT,()=>{
     console.log(`Listening at port ${process.env.PORT}`);
